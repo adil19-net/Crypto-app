4a82e47be48b264da7ec87baa25c120054cdcc3c
Adil <adilvinayak19@gmail.com>
2022-06-16 12:42:24 +0530

    ANother commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import {
-  HashRouter as Router,
-} from "react-router-dom";
-
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 import Home from "./Routes/Home";
 import CoinPage from "./Routes/CoinPage";
